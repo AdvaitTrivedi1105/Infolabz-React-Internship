a12a3117302c9043655cab0c7aa2dd40dcd85f18
advaittrivedi1122 <trivediadvait0@gmail.com>
2022-06-27 00:14:29 +0530

    Mutual Funds API assignment added

---
+++ Assignment 1/Mutual Fund/mutual_fund.js
@@ -0,0 +1,27 @@
+async function load(){
+
+    const url = "https://api.mfapi.in/mf";
+    let data = await (await fetch(url)).json();
+
+
+    const total = document.getElementById('total');
+    const code = document.getElementById('code').value;
+    const name = document.getElementById('name');
+    let check = false;
+
+    const dataLength = data.length;
+    total.innerText = dataLength;
+
+
+    for(let i=0;i<dataLength;i++){
+        if(data[i]["schemeCode"] == code){
+            name.innerText = data[i]["schemeName"];
+            check = true;
+        }
+    }
+
+    if (check == false){
+        name.innerText = "schemeCode not available in API";
+    }
+
+}
